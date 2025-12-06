0c32d19e909a39377a3def8f8a055c10c1d24b82
Elbetel Molla <elbetelmolla25@gmail.com>
2025-12-06 22:38:59 +0300

    update file content

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 from src.eda import load_data, basic_info, univariate_analysis, bivariate_analysis, correlation_heatmap
 
-NUMERIC_COLS = ['age', 'bmi', 'children', 'charges']
-DATA_PATH = "data/insurance_data.csv"
-PLOT_DIR = "plots"
-
 def main():
-    df = load_data(DATA_PATH)
-    if df is None:
-        return
-
-    # Basic info
-    basic_info(df)
-
-    # Univariate analysis
-    univariate_analysis(df, NUMERIC_COLS, PLOT_DIR)
-
-    # Bivariate analysis
-    bivariate_analysis(df, PLOT_DIR)
-
-    # Correlation heatmap
-    correlation_heatmap(df, NUMERIC_COLS, PLOT_DIR)
+    df = load_data("data/insurance_data.csv")  # pipe-separated text file
+    if df is not None:
+        basic_info(df)
+        univariate_analysis(df)
+        bivariate_analysis(df)
+        correlation_heatmap(df)
 
 if __name__ == "__main__":
     main()
